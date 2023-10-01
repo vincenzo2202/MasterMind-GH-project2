30e87562291a18a265435af32d8f35991da03bc4
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-10-01 18:35:36 +0200

    Fix: Fix color picker interaction by correcting the color array and selection in js page for colors

---
+++ js/colors.js
@@ -0,0 +1,49 @@
+// Se declaran las variables 
+
+const color1 = document.getElementById('colors1');
+const color2 = document.getElementById('colors2');
+const color3 = document.getElementById('colors3');
+const color4 = document.getElementById('colors4');
+const colorSelected = document.getElementsByClassName('choice-color');
+let lastSelectedColor = null;
+
+// Función para aplicar el color al elemento seleccionado
+
+
+
+// desplegable 
+color1.addEventListener('click', () => {
+    collapseBox.classList.toggle('expanded');
+    lastSelectedColor = color1;
+});
+
+color2.addEventListener('click', () => {
+    collapseBox.classList.toggle('expanded');
+    lastSelectedColor = color2;
+});
+
+color3.addEventListener('click', () => {
+    collapseBox.classList.toggle('expanded');
+    lastSelectedColor = color3;
+});
+
+color4.addEventListener('click', () => {
+    collapseBox.classList.toggle('expanded');
+    lastSelectedColor = color4;
+});
+
+
+function applyColorToElement(element, color) {
+    element.style.backgroundColor = color;
+}
+
+// Agregar eventos a los elementos con la clase 'choice-color'
+for (let i = 0; i < colorSelected.length; i++) {
+    colorSelected[i].addEventListener('click', () => {
+        if (lastSelectedColor) {
+            const selectedColor = getComputedStyle(colorSelected[i]).backgroundColor;
+            applyColorToElement(lastSelectedColor, selectedColor);
+        }
+    });
+}
+ 
--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
-// -------------------- Callapse Window colors selector
-const colors = document.getElementsByClassName('colors');
-const choiceColors = document.getElementsByClassName('choice-color');
-
-for (let i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', () => {
-        collapseBox.classList.toggle('expanded');
-    });
-
-
-    choiceColors[i].addEventListener('click', () => {
-
-        const colorBackground = getComputedStyle(choiceColors[i]).backgroundColor;
-
-
-        colors[i].style.backgroundColor = colorBackground;
-    });
-}
-
-// -------------------- Callapse Window Rules Game
+ 
+// -------------------- Callapse Window Rules Game 
 const rulesCollapseButton = document.getElementById('rulesCollapseButton');
-const collapseRules = document.getElementById('collapseRules');
 
 rulesCollapseButton.addEventListener('click', () => {
     collapseRules.classList.toggle('expanded');
+    
+});  
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const collapseRules = document.getElementById('collapseRules');
+    collapseRules.classList.remove('expanded');
 });
