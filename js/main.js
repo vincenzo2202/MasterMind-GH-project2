2d32ea19ddc7ca2855347d85077e3550cf31e78f
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-10-02 00:45:41 +0200

    feat: Add functionality for selected colors to be retrieved from local storage to the game board

--- js/main.js
+++ js/main.js
@@ -5,10 +5,31 @@ const rulesCollapseButton = document.getElementById('rulesCollapseButton');
 rulesCollapseButton.addEventListener('click', () => {
     collapseRules.classList.toggle('expanded');
     
-});  
-
+});   
 
 document.addEventListener('DOMContentLoaded', () => {
     const collapseRules = document.getElementById('collapseRules');
     collapseRules.classList.remove('expanded');
 });
+
+// coger los elementos del localstorage 
+
+const colors1 = localStorage.getItem('colors1');
+const colors2 = localStorage.getItem('colors2');
+const colors3 = localStorage.getItem('colors3');
+const colors4 = localStorage.getItem('colors4');
+
+// Función para aplicar un color a un elemento
+function applyColorToElement(element, color) {
+    if (color) {
+        element.style.backgroundColor = color;
+    }
+}
+
+// Aplica los colores a los elementos correspondientes
+applyColorToElement(document.getElementById('selected1'), colors1);
+applyColorToElement(document.getElementById('selected2'), colors2);
+applyColorToElement(document.getElementById('selected3'), colors3);
+applyColorToElement(document.getElementById('selected4'), colors4);
+
+
